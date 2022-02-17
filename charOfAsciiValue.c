ef6c7d86e657e6eb4d56e40f1d79ae9bed85fdc0
unknown <rdg01834@gmail.com>
2022-02-17 23:53:27 +0600

    print character of an ASCII value

---
+++ charOfAsciiValue.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int n;
+
+    printf("Enter ASCII Value: ");
+    scanf("%d",&n);
+
+    printf("The Character of The ASCII Value Is: %c",n);
+
+
+    return 0;
+
+}
