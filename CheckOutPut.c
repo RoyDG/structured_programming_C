4139c8c08233df4263ea408165c03d72ac16c46a
unknown <rdg01834@gmail.com>
2022-02-25 11:40:40 +0600

    find output

---
+++ CheckOutPut.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int i =10,b=i;
+    printf("\n%d",b);
+    i=i+10;
+    --i;
+    printf("\n%d",i);
+    b=i;
+    printf("\nHello world:%d",i);
+    printf("\n%d",b);
+    b++;
+    printf("\n%d",b);
+
+    return 0;
+}
