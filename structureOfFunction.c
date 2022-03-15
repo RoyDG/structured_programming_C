4d16c7a153838bd91e4777b0225a5ed32caebc96
unknown <rdg01834@gmail.com>
2022-03-15 18:47:24 +0600

    sum of two number using function

--- structureOfFunction.c
+++ structureOfFunction.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
-    int sum (int, int);//function declaration
+    int sum (int a, int b) {
+	    int s = a+b;
+        return s;
+    }
+
+
     int main (){
         int total;
-		printf("\nstructure of function :\n");
+        int x,y;
+		printf("Enter two number :");
+		scanf("%d%d",&x,&y);
+        total = sum (x,y);
+        printf ("The total sum is: %d\n",total);
 
-        total = sum (5, 6);//function call
-        printf ("The total is :  %d\n", total);
         return 0;
     }
 
-    int sum (int a, int b) //function definition
-    {
-	    int s = a+b;
-        return s; //function returning a value
-    }
