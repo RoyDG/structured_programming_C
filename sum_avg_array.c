d43112c4e3824b8ec4cd6e6d266b48a030bc0202
unknown <rdg01834@gmail.com>
2022-03-19 21:05:34 +0600

    sum & avg of an array optimized

--- sum_avg_array.c
+++ sum_avg_array.c
@@ -4,18 +4,17 @@ int main(){
 
     int sum = 0, i, size;
 
-    printf("Enter Array Size : \n");
+    printf("Enter Array Size : ");
     scanf("%d",&size);
+
     int arr[size];
+    printf("\nArray Elements : ");
 
     for (i=0; i<size; i++){
         scanf("%d",&arr[i]);
-    }
-
-    for (i=0; i<size; i++){
         sum = sum + arr[i];
     }
-    printf("The sum is : %d\n",sum);
+    printf("\nThe sum is : %d\n",sum);
     printf("The avg is : %.2f\n",(float)sum/size);
 
 }
