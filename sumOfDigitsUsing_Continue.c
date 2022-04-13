1ae8144f966e89c82b42bc532f6126ecd84fec9a
unknown <rdg01834@gmail.com>
2022-04-13 14:51:19 +0600

    sum of digit using continue

---
+++ sumOfDigitsUsing_Continue.c
@@ -0,0 +1,24 @@
+// Program to calculate the sum of digits
+// If the user enters a negative number, it's not added to the result
+
+#include <stdio.h>
+
+
+int main() {
+    int i;
+    double number, sum = 0.0;
+    printf("Enter number: ");
+    scanf("%lf", &number);
+
+    for (i = 1; i <= number; ++i){
+        if (number < 0.0){
+        continue;
+    }
+
+    sum = sum + i;
+    }
+
+    printf("Sum = %.2lf", sum);
+
+    return 0;
+}
