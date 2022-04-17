f4d9cc992f0f7bea0eb2fefe0a769130af2aff9e
unknown <rdg01834@gmail.com>
2022-04-17 14:00:19 +0600

    bug fixed

--- str_count_num_char_spChar.c
+++ str_count_num_char_spChar.c
@@ -1,43 +1,33 @@
 #include <stdio.h>
-#define MAX_SIZE 100 // Maximum string size
 
 
-int main()
-{
-    char str[MAX_SIZE];
+int main(){
+    char str[100];
     int alphabets, digits, others, i;
 
     alphabets = digits = others = i = 0;
 
-
-    /* Input string from user */
     printf("Enter any string : ");
     gets(str);
 
-    /*
-     * Check each character of string for alphabet, digit or special character
-     */
-    while(str[i]!='\0')
-    {
-        if((str[i]>='a' && str[i]<='z') || (str[i]>='A' && str[i]<='Z'))
-        {
+    while(str[i]!='\0'){
+        if((str[i]>='a' && str[i]<='z') || (str[i]>='A' && str[i]<='Z')){
             alphabets++;
         }
-        else if(str[i]>='0' && str[i]<='9')
-        {
+        else if(str[i]>='0' && str[i]<='9'){
             digits++;
         }
-        else
-        {
+        else{
             others++;
         }
 
         i++;
     }
 
-    printf("Alphabets = %d\n", alphabets);
-    printf("Digits = %d\n", digits);
-    printf("Special characters = %d", others);
+    printf("\n    Alphabets = %d\n", alphabets);
+    printf("    Digits = %d\n", digits);
+    printf("    Special characters = %d", others);
 
+    getch();
     return 0;
 }
