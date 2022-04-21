a89ed310a4a2f25b4521a839864c24af6c96ff3f
unknown <rdg01834@gmail.com>
2022-04-21 23:56:32 +0600

    even or odd using bitwise

---
+++ even_odd_bitwise.c
@@ -0,0 +1,15 @@
+/* C program to check whether a number is even or odd using bitwise operator */
+
+#include <stdio.h>
+
+int main(){
+    int num;
+    printf("Enter any number: ");
+    scanf("%d", &num);
+
+    (num & 1)
+        ? printf("%d is odd.", num)
+        : printf("%d is even.", num);
+
+    return 0;
+}
