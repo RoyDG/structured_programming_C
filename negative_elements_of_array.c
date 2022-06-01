526cb83616ad26c41f2807583783248fbc392901
unknown <rdg01834@gmail.com>
2022-06-01 23:46:29 +0600

    print all negative elements of array

---
+++ negative_elements_of_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define MAX_SIZE 100
+
+int main(){
+    int arr[MAX_SIZE];
+    int i, n;
+
+    printf("Enter size of the array : ");
+    scanf("%d", &n);
+
+    printf("Enter elements in array : ");
+    for(i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\nAll negative elements in array are : ");
+    for(i=0; i<n; i++){
+        if(arr[i] < 0){
+            printf("%d\t", arr[i]);
+        }
+    }
+
+    return 0;
+}
